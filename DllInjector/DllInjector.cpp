ec0df3e5e20ffc87d07faa1b52ef5fdecbc886d7
Anastasiia Terenteva <terenteva8412@gmail.com>
2020-04-27 20:33:45 +0300

    Refactored in unit test style.

--- DllInjector/DllInjector.cpp
+++ DllInjector/DllInjector.cpp
@@ -4,23 +4,60 @@
 #include <stdio.h>
 #include "../myapi.h"
 
-int main()
+void TestStaticImport()
 {
-    auto r = sum(2, 3);
+    const auto r = sum(2, 3);
     printf("sum returns %d \n", r);
+}
 
+HMODULE TestDirectLoadLibarary()
+{
     const auto hLibrary = LoadLibraryW(L"DllToInject.dll");
     if (nullptr == hLibrary)
     {
-        printf("LoadLibraryW returns %lu \n", GetLastError());
+        printf("LoadLibraryW failed %lu \n", GetLastError());
     }
+    return hLibrary;
+}
+
+HMODULE TestLocalThreadLoadLibrary32()
+{
+    DWORD tid, exitCode;
+    const auto pLoadLibraryW = reinterpret_cast<LPTHREAD_START_ROUTINE>(&LoadLibraryW);
+    auto lpParameter = const_cast<wchar_t*>(L"DllToInject.dll");
+    const auto hThread = CreateThread(nullptr, 0, pLoadLibraryW, lpParameter, 0, &tid);
+    if (nullptr == hThread)
+    {
+        printf("CreateThread failed %lu \n", GetLastError());
+        return nullptr;
+    }
+    WaitForSingleObject(hThread, INFINITE);
+    GetExitCodeThread(hThread, &exitCode);
+    CloseHandle(hThread);
+#ifndef WIN64
+    return reinterpret_cast<HMODULE>(exitCode); //32-bit only magic
+#else
+    return nullptr;
+#endif
+}
+
+void TestDynamicImport(HMODULE hLibrary)
+{
+    if (nullptr == hLibrary) return;
     const auto pMul = reinterpret_cast<PFN_mul>(GetProcAddress(hLibrary, "_mul@8"));
     if (nullptr == pMul)
     {
-        printf("GetProcAddress returns %lu \n", GetLastError());
+        printf("GetProcAddress failed %lu \n", GetLastError());
     }
-    r = pMul(2, 3);
+    const auto r = pMul(2, 3);
     printf("mul returns %d \n", r);
+}
+
+int main()
+{
+    TestStaticImport();
+    TestDynamicImport(TestDirectLoadLibarary());
+    TestDynamicImport(TestLocalThreadLoadLibrary32());
 
     return 0;
 }
